19c115e235baba71276fb0fb44515abcea96173a
Andrej <anhr@mail.ru>
2020-09-17 11:07:31 +0700

    MoveGroup
    import Cookie

--- MoveGroup.js
+++ MoveGroup.js
@@ -18,9 +18,8 @@ import ScaleController from './ScaleController.js';
 import PositionController from './PositionController.js';
 import { dat } from './dat/dat.module.js';
 
-//import Cookie from '../../cookieNodeJS/master/cookie.js';
-//import Cookie from 'https://raw.githack.com/anhr/cookieNodeJS/master/cookie.js';
-import Cookie from 'https://raw.githack.com/anhr/commonNodeJS/master/cookieNodeJS/cookie.js';
+import Cookie from './cookieNodeJS/cookie.js';
+//import Cookie from 'https://raw.githack.com/anhr/commonNodeJS/master/cookieNodeJS/cookie.js';
 
 /**
  * Change group's position, scale and rotation.
