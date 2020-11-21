67839cbbc5b3a0459e04b63a78fba99c09e24630
Andrej <anhr@mail.ru>
2020-11-21 21:34:56 +0700

    CameraGui

--- CameraGui.js
+++ CameraGui.js
@@ -1,7 +1,8 @@
 /**
- * [Camera]{@link https://threejs.org/docs/index.html#api/en/cameras/PerspectiveCamera} graphical user interface
- *
- * @author Andrej Hristoliubov https://anhr.github.io/AboutMe/
+ * @module CameraGui
+ * @description [Camera]{@link https://threejs.org/docs/index.html#api/en/cameras/PerspectiveCamera} graphical user interface.
+ * 
+ * @author [Andrej Hristoliubov]{@link https://anhr.github.io/AboutMe/}
  *
  * @copyright 2011 Data Arts Team, Google Creative Lab
  *
@@ -15,12 +16,17 @@
 //import PositionController from './PositionController.js';
 import { dat } from './dat/dat.module.js';
 
+//import Player from './player/player.js';
+//import Player from 'https://raw.githack.com/anhr/commonNodeJS/master/player/player.js';
+
 /**
  * Camera graphical user interface
  * @param {GUI} gui is [new dat.GUI(...)]{@link https://github.com/anhr/dat.gui}.
  * @param {THREE.PerspectiveCamera} camera [PerspectiveCamera]{@link https://threejs.org/docs/index.html#api/en/cameras/PerspectiveCamera}
  * @param {object} [options] the following options are available:
  * @param {object} [options.scales={}] axes scales.
+ * See [AxesHelper(...)]{@link https://raw.githack.com/anhr/commonNodeJS/master/AxesHelper/jsdoc/module-AxesHelper.html} options.scales for details.
+ * @param {Function} [options.getLanguageCode="en"] returns the "primary language" subtag of the version of the browser. Default returns "en" is English
 */
 var CameraGui = function ( gui, camera, options ) {
 
@@ -31,23 +37,59 @@ var CameraGui = function ( gui, camera, options ) {
 
 	options = options || {};
 
+	//scales
+
+	options.scales = options.scales || {};
+
+	options.scales.x = options.scales.x || {};
+	options.scales.x.name = options.scales.x.name || 'X';
+
+	options.scales.y = options.scales.y || {};
+	options.scales.y.name = options.scales.y.name || 'Y';
+
+	options.scales.z = options.scales.z || {};
+	options.scales.z.name = options.scales.z.name || 'Z';
+
+	//
+
+	if ( options.orbitControls )
+		options.orbitControls.addEventListener( 'change', function () {
+
+			update();
+
+		} );
+
 	//Localization
 
-	var lang = {
+	const lang = {
 
 		camera: 'Camera',
+
+		position: 'Position',
+		positionTitle: 'Camera position',
+
+		distanceToCamera: 'Distance',
+		distanceToCameraTitle: 'Distance from the camera to the point at which the camera is looking',
+
 		defaultButton: 'Default',
 		defaultTitle: 'Restore default camera settings.',
 
 	};
 
-	var _languageCode = options.getLanguageCode === undefined ? 'en'//Default language is English
+	const _languageCode = options.getLanguageCode === undefined ? 'en'//Default language is English
 		: options.getLanguageCode();
 	switch ( _languageCode ) {
 
 		case 'ru'://Russian language
 
-			lang.camera = 'Камера',
+			lang.camera = 'Камера';
+
+			lang.position = 'Позиция';
+			lang.positionTitle = 'Позиция камеры';
+
+			lang.distanceToCamera = 'Расстояние',
+			lang.distanceToCameraTitle = 'Расстояние между камерой и точкой, на которую она смотрит.',
+
 			lang.defaultButton = 'Восстановить';
 			lang.defaultTitle = 'Восстановить настройки камеры по умолчанию.';
 
@@ -66,17 +108,144 @@ var CameraGui = function ( gui, camera, options ) {
 
 	}
 
-	var fCamera = gui.addFolder( lang.camera );
+	const fCamera = gui.addFolder( lang.camera ),
+		fPosition = fCamera.addFolder( lang.position ),
+		controllersPosition = {
+
+			x: dat.controllerZeroStep( fPosition, camera.position, 'x', function ( value ) {
+
+				camera.position.x = value;
+
+			} ),
+			y: dat.controllerZeroStep( fPosition, camera.position, 'y', function ( value ) {
+
+				camera.position.y = value;
+
+			} ),
+
+			z: dat.controllerZeroStep( fPosition, camera.position, 'z', function ( value ) {
+
+				camera.position.z = value;
+
+			} ),
+
+		};
+	dat.folderNameAndTitle( fPosition, lang.position, lang.positionTitle );
+	dat.controllerNameAndTitle( controllersPosition.x, options.scales.x.name );
+	dat.controllerNameAndTitle( controllersPosition.y, options.scales.y.name );
+	dat.controllerNameAndTitle( controllersPosition.z, options.scales.z.name );
+
+	var controllersDistance, defaultDistance;
+	if ( camera.userData.cameraTarget ) {
+
+		const Player = camera.userData.cameraTarget.Player,
+			fDistanceToCamera = fCamera.addFolder( lang.distanceToCamera ),
+			distance = {
+
+				x: Player.execFunc( camera.userData.cameraTarget.distanceToCamera, 'x' ),
+				y: Player.execFunc( camera.userData.cameraTarget.distanceToCamera, 'y' ),
+				z: Player.execFunc( camera.userData.cameraTarget.distanceToCamera, 'z' ),
+				
+			};
+			function setDistance(){
+
+				camera.userData.cameraTarget.setCameraPosition( camera.userData.cameraTarget.target );
+				update();
+				
+			}
+			controllersDistance = {
+
+				x: dat.controllerZeroStep( fDistanceToCamera, distance, 'x', function ( value ) {
+
+					camera.userData.cameraTarget.distanceToCameraCur.x = value;
+					setDistance();
+
+				} ),
+				y: dat.controllerZeroStep( fDistanceToCamera, distance, 'y', function ( value ) {
+
+					camera.userData.cameraTarget.distanceToCameraCur.y = value;
+					setDistance();
+
+				} ),
+
+				z: dat.controllerZeroStep( fDistanceToCamera, distance, 'z', function ( value ) {
+
+					camera.userData.cameraTarget.distanceToCameraCur.z = value;
+					setDistance();
+
+				} ),
+
+			};
+		defaultDistance = { x: distance.x, y: distance.y, z: distance.z };
+		dat.folderNameAndTitle( fDistanceToCamera, lang.distanceToCamera, lang.distanceToCameraTitle );
+		dat.controllerNameAndTitle( controllersDistance.x, options.scales.x.name );
+		dat.controllerNameAndTitle( controllersDistance.y, options.scales.y.name );
+		dat.controllerNameAndTitle( controllersDistance.z, options.scales.z.name );
+	
+	}
 
 	//Default button
+	const defaultPosition = { x: camera.position.x, y: camera.position.y, z: camera.position.z },
+		defaultTarget = options.orbitControls ?
+		{
+
+			x: options.orbitControls.target.x,
+			y: options.orbitControls.target.y,
+			z: options.orbitControls.target.z,
+
+		} :
+		undefined;
 	dat.controllerNameAndTitle( fCamera.add( {
 
 		defaultF: function ( value ) {
 
+			controllersPosition.x.setValue( defaultPosition.x );
+			controllersPosition.y.setValue( defaultPosition.y );
+			controllersPosition.z.setValue( defaultPosition.z );
+
+			camera.position.copy( defaultPosition );
+			camera.lookAt( defaultTarget );
+			if ( options.orbitControls ) {
+
+				options.orbitControls.target.copy( defaultTarget );
+				options.orbitControls.update();
+
+			}
+
+			controllersDistance.x.setValue( defaultDistance.x );
+			controllersDistance.y.setValue( defaultDistance.y );
+			controllersDistance.z.setValue( defaultDistance.z );
 
 		},
 
 	}, 'defaultF' ), lang.defaultButton, lang.defaultTitle );
+
+	function update(){
+
+		controllersPosition.x.setValue( camera.position.x );
+		controllersPosition.y.setValue( camera.position.y );
+		controllersPosition.z.setValue( camera.position.z );
+
+		if ( controllersDistance ) {
+
+			controllersDistance.x.setValue( camera.userData.cameraTarget.distanceToCameraCur.x );
+			controllersDistance.y.setValue( camera.userData.cameraTarget.distanceToCameraCur.y );
+			controllersDistance.z.setValue( camera.userData.cameraTarget.distanceToCameraCur.z );
+
+		}
+
+	}
+	if ( this ) {
+
+		/**
+		 * Update camera controls.
+		 * @function CameraGui.
+		 * update
+		 */
+		this.update = function () { update(); }
+		camera.userData.cameraTarget.cameraGui = this;
+
+	}
 
 }
 
