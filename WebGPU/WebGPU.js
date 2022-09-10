22e6e5f58b27fd929d40ab5dfbea16e32cf2b6c2
anhr <anhr@mail.ru>
2022-09-10 08:48:35 +0700

    WebGPU

---
+++ WebGPU/WebGPU.js
@@ -0,0 +1,25 @@
+/**
+ * @module webGPU
+ * @description [GPU Compute on the web]{@link https://web.dev/gpu-compute/}.
+ * @see [webGPU]{@link https://gpuweb.github.io/gpuweb/}
+ *
+ * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+class webGPU {
+
+    constructor() {
+
+    }
+
+}
+
+export default webGPU;
