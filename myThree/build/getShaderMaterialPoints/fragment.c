456f16bfd8244ffc649ab30762fd2900c357e262
anhr <anhr@mail.ru>
2023-03-14 19:16:57 +0700

    myThree\build

--- myThree/build/getShaderMaterialPoints/fragment.c
+++
@@ -1,7 +0,0 @@
-//uniform vec4 color;
-uniform sampler2D pointTexture;
-varying vec4 vColor;
-void main() {
-	vec4 color = vec4( vColor ) * texture2D( pointTexture, gl_PointCoord );
-	gl_FragColor = color;
-}
