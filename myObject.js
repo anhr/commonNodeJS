41496d191ac7e0e54aa9294877d479d03325ccbd
anhr <anhr@mail.ru>
2024-07-27 10:06:44 +0700

    Update myObject.js

--- myObject.js
+++ myObject.js
@@ -162,7 +162,8 @@ class MyObject {
 									case 'toJSON': return (item) => {
 
 										let res = '[';
-										vertice.forEach(axis => { res += axis + ', ' })
+//										vertice.forEach(axis => { res += axis + ', ' });
+										verticeProxy.forEach(axis => { res += axis + ', ' });
 										return res.substring(0, res.length-2) + ']';
 											
 									}
