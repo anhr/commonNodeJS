21bfa20b2edb6c71e0b317c97c930e589e6ae6ff
anhr <anhr@mail.ru>
2024-12-15 10:51:04 +0700

    onSelectScene 4
    for playing in http://localhost/anhr/commonNodeJS/master/HyperSphere/Examples/hyperSphere.html

--- HyperSphere/hyperSphere.js
+++ HyperSphere/hyperSphere.js
@@ -270,7 +270,7 @@ class HyperSphere extends MyObject {
 	constructor(options, classSettings = {}) {
 
 		classSettings.onSelectScene ||= (hyperSphere, timeId, t) => { if (this.middleVertices) return this.middleVertices(timeId, t); }
-/*		
+		//for playing in http://localhost/anhr/commonNodeJS/master/HyperSphere/Examples/hyperSphere.html
 		options.onSelectScene = (index, t) => {
 
 			if (classSettings.onSelectScene) return classSettings.onSelectScene(this, index, t);
@@ -279,7 +279,6 @@ class HyperSphere extends MyObject {
 			return true;//Сдедующий шаг проигрывателя выполняется только после посторения всех вершин без временной задержки
 		
 		}
-*/			
 		classSettings.settings = classSettings.settings || {};
 		if (classSettings.debug) classSettings.settings.debug = classSettings.debug;
 		classSettings.settings.options = options;
