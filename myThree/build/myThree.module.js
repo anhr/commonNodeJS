f4fc22172a7af0d8a1b02dc98a106d8f57709897
Andrej <anhr@mail.ru>
2025-02-16 07:39:32 +0700

    myThree\build

--- myThree/build/myThree.module.js
+++ myThree/build/myThree.module.js
@@ -18885,10 +18885,16 @@ class MyObject {
 			}
 
 			//gui
-			const guiSelectPoint = settings.options.guiSelectPoint, object3D = this.object ? this.object() : this.object3D;
-			if (guiSelectPoint && (guiSelectPoint.getSelectedPointIndexShort() === i) && guiSelectPoint.isSelectedMesh(object3D))
-				guiSelectPoint.setPosition( { index: i, object: this.object ? this.object() : this.object3D });
-			if (object3D && object3D.userData.gui) object3D.userData.gui.reset();//в hyperSphere обновить выделенные ребра, среднюю вершину и плоскости вращения углов
+			const guiSelectPoint = settings.options.guiSelectPoint,
+				object3D = this.object3D;
+//				object3D = this.object ? this.object() : this.object3D;
+			if (guiSelectPoint && (guiSelectPoint.getSelectedPointIndexShort() === i) && guiSelectPoint.isSelectedMesh(object3D)) {
+				
+				guiSelectPoint.setPosition( { index: i, object: object3D });
+//				guiSelectPoint.setPosition( { index: i, object: this.object ? this.object() : this.object3D });
+				if (object3D && object3D.userData.gui) object3D.userData.gui.reset();//в hyperSphere обновить выделенные ребра, среднюю вершину и плоскости вращения углов
+
+			}
 
 			//Color attribute
 
