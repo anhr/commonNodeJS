8ca8cedd73ed34ec47e5e30bae562371720fbe8f
Andrej <anhr@mail.ru>
2025-03-07 13:25:49 +0700

    Update nD.js
    Экономия памяти

--- nD/nD.js
+++ nD/nD.js
@@ -2066,11 +2066,17 @@ class ND extends MyObject {
 			},
 
 		}
-		const points = [];
-		for ( var i = 0; i < geometry.position.length; i++ )
-			points.push( geometry.position[i].point );
-		this.getPoint = (i) => { return points[i]; }
-		this.setPositionAttributeFromPoints( points );
+		{//delete points from memory
+			
+			const points = [];
+			for ( var i = 0; i < geometry.position.length; i++ )
+				points.push( geometry.position[i].point );
+			this.getPoint = (i) => { return points[i]; }//Эта функция используется для создания Position Attribute. Затем эта функция заменяется на функцию ниже. Это делается для того, что бы избавиться от массива points, который дублирует Position Attribute
+			this.setPositionAttributeFromPoints( points );
+			points.length = 0;
+			this.getPoint = (i) => { return settings.bufferGeometry.userData.position[i]; }
+			
+		}
 
 		vectorPlane = vectorPlane || new Vector( settings.vectorPlane );
 		if ( !vectorPlane || !vectorPlane.point ) vectorPlane = new Vector( vectorPlane );
