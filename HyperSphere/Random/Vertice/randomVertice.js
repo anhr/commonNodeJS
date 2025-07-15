040abb62cdf3579643fa4dfe59b0451c83f7e5c5
Andrej <anhr@mail.ru>
2025-07-15 07:50:21 +0700

    Add RandomVertice

---
+++ HyperSphere/Random/Vertice/randomVertice.js
@@ -0,0 +1,53 @@
+/**
+ * @module RandomVertice
+ * @description Generates random angles between a vertice and its opposite vertice.
+ *
+ * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+const sRandomVertice = 'RandomVertice',
+	sOver = ': Please, override %s method in your ' + sRandomVertice + ' child class.';
+/**
+ * Generates random angles between a vertice and its opposite vertice.
+ * @class
+ */
+class RandomVertice {
+
+	/**
+	 * Generates random angles between a vertice and its opposite vertice.
+	 * @param {object} [params={}] The following parameters are available.
+	 * @param {Array} [params.vertice=[0, 0, 0]] First vertice of the arc between two vertices.
+	 * @param {Array} [params.oppositeVertice=[0, 0, 0]] Second vertice of the arc between two vertices.
+	 */
+	constructor(params) {
+
+		this.params = params;
+		params.vertice ||= this.ZeroArray();
+		params.oppositeVertice ||= this.ZeroArray();
+		this.Center(params);
+/*		
+		this.angles = () => {
+			
+			console.log(params)
+			return [0];
+			
+		}
+*/		
+		
+	}
+	
+	//overridden methods
+	
+	get angles() { console.error(sRandomVertice + sOver.replace('%s', 'get angles')) }
+	
+	/////////////////////////////overridden methods
+
+}
+export default RandomVertice;
