660a76dc2a46c1d0d40bc42bd542618029c702a2
Andrej <anhr@mail.ru>
2025-09-03 08:08:00 +0700

    HyperSphere\RandomVertice\jsdoc

---
+++ HyperSphere/RandomVertice/getHyperSphere.js
@@ -0,0 +1,15 @@
+/**
+ * @module getHyperSphere
+ * @description Returns a <a href="../../../../master/HyperSphere/jsdoc/module-HyperSphere-HyperSphere.html" target="_blank">Hypersphere</a> with random vertices.
+ *
+ * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+*/
+
